b38a6e08e78a26a4bafb21a72aa938f6384c1ebd
Jacques Mostert <design.signature@gmail.com>
2022-04-24 00:21:10 +0200

    15 basic page setup (#16)
    * Remove about.html
    
    * Updating home page,  library page and adding blog page.
    
    Post example page has also been modified, but you cannot navigate to it at the moment.
    
    * Remove page partials
    
    * - Remove function that includes page partials
    - Check and set localstorage for theme usage
    - Include theme toggle
    
    * Strictly experimental thing based on a Kevin Powell video - spinning nav
    
    * Update jayx-github-io.code-workspace
    
    * Adding a bunch of icons to be used in library CSS
    
    * Update jayx-github.sketch
    
    * Commenting out everything but the library CSS in app theme file
    
    * Adding images to library CSS folder for use as banners and icons
    
    * Update vars.css - setting library palette and light/dark theme values from palette
    
    * Update reset.css - we don't need "support for older browsers"
    
    * Update scaffolding.css - set basic styles (minimal required)
    
    * Update typography.css with base typography styles
    
    * Delete containers.css - who still needs to clear floats?
    
    * Delete grid.css - this thing was also based on floats (old school)
    
    * Update page-layout.css - for now we're stashing all the things in here to move to appropriate locatiosn later
    
    * Update icons.css - icons ... we got your icons
    
    * Update utilities.css - basically, if it isn't in page-layout, it's in utilities for now
    
    * Update gist.css - the beginnings of a Github Gist theme for on-page code snippets
    
    * Update base.css - removing font references, but this thing is probably next to get the chop
    
    * Delete header.css - now in page-layout
    
    * Update blox.css - putting it all together in on place

--- js/jayx.js
+++ js/jayx.js
@@ -1,8 +1,15 @@
 // Load navigation from one location
-$(document).ready(function(){
-  $('.site-header').load('partials/site-header.html .heading');
-  $('#main-nav').load('partials/nav.html #nav-main');
-});
+// $(document).ready(function(){
+//   $('#site-header').load('partials/site-header.html');
+//   $('#site-footer').load('partials/site-footer.html');
+//   $('#main-nav').load('partials/nav.html nav');
+// });
+
+// Check localstorage for theme, else set theme and localstorage
+var html = document.getElementsByTagName("html")[0];
+html.setAttribute("theme", localStorage.getItem('theme') || 'light');
+
+
 // Adjust header
 var headerHeight = $('header').height() / 2;
 $(document).scroll(function(e){
@@ -13,4 +20,41 @@ $(document).scroll(function(e){
   } else {
     $('html').removeClass('zoom--out');
   }
+});
+
+$(document).ready(function(){
+
+
+  
+  // switch between light/dark themes
+  toggleTheme = function () {
+    var theme = localStorage.getItem('theme') !== 'dark' ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+    applyTheme(theme);
+  };
+
+  // change css href to apply a theme
+  applyTheme = function (theme) {
+    if (theme === 'dark') {
+      $('html').attr('theme', 'dark');
+    } else {
+      $('html').attr('theme', 'light');
+    }
+    $(document).trigger('themeChanged.bc.themeToggle', theme);
+  };
+
+  // listen for theme change in any other tab
+  window.addEventListener('storage', function (e) {
+    if (e.key === 'theme') {
+      applyTheme(e.newValue);
+    }
+  });
+
+  // theme toggler functionality
+  $('[data-toggle-theme]').on('click', function () {
+    console.log("theme")
+    toggleTheme();
+    return false;
+  });
+
 });
